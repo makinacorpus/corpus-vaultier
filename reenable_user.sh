719255c5a44b0051311ff6a4d8f9896b9a08b948
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-10-21 10:57:13 +0200

    renable feature

---
+++ reenable_user.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+cd $(dirname $0)
+users=$@
+if [[ -z $users ]];then
+        echo "$0 user"
+        exit 1
+fi
+export DJANGO_SETTINGS_MODULE=vaultier.vaultier_settings
+if [ -e venv/bin/activate ];then
+    . venv/bin/activate
+fi
+
+cd vaultier/vaultier
+dbsetting() {
+    python -c "import $DJANGO_SETTINGS_MODULE;print($DJANGO_SETTINGS_MODULE.DATABASES['default']['$1'])"
+}
+export PGPASSWD=$(dbsetting PASSWORD)
+export PGPASSWORD=$(dbsetting PASSWORD)
+for user in $users;do
+    echo "update vaultier_user set nickname ='$user', email='${user}@makina-corpus.com' where nickname='old2$user';" | ./manage.py dbshell
+done 
+# vim:set et sts=4 ts=4 tw=80:
